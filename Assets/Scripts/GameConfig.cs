e8407b083f3d267e737e876410f98129d200888f
Виталий Лукин <mr.lukin1990@mail.ru>
2020-11-11 00:08:21 +0300

    Initial commit

---
+++ Assets/Scripts/GameConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GameConfig
+{
+    public int gameAreaWidth;
+	public int gameAreaHeight;
+	public int numUnitsToSpawn;
+	public int unitSpawnDelay;
+	public float unitSpawnMinRadius;
+	public int unitSpawnMaxRadius;
+	public int unitSpawnMinSpeed;
+	public int unitSpawnMaxSpeed;
+	public float unitDestroyRadius;
+
+	//Сериализация GameConfig.json в объект
+	public static GameConfig StartSer()
+    {
+		string path = Application.streamingAssetsPath + "\\GameConfig.json";
+		string text = File.ReadAllText(path);
+
+		GameConfig config = JsonUtility.FromJson<GameConfig>(text);
+
+		return config;
+    }
+}
+
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         GameConfig config = GameConfig.StartSer();
+        Debug.Log(config.gameAreaHeight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
